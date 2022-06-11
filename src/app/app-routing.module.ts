bdacc2698a61555a247842d84c34dd50f05bf7ee
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-06-11 19:39:43 +0200

    feat: add Title accessibility example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,31 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {Injectable, NgModule} from '@angular/core';
+import {ActivatedRouteSnapshot, RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {TypedFormComponent} from "./typed-form/typed-form.component";
+import {map, timer} from "rxjs";
+
+
+@Injectable({ providedIn: 'root' })
+export class HomeTitle {
+  resolve(_route: ActivatedRouteSnapshot) {
+    return timer(1000).pipe(map(() => `Home ${new Intl.DateTimeFormat().format(new Date())}`));
+  }
+}
+
 
 const routes: Routes = [
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    // NEW: Title accessibility
+    // NOTE: waits until title is resolved
+    title: HomeTitle
   },
   {
     path: 'typed-reactive-form',
-    component: TypedFormComponent
+    component: TypedFormComponent,
+    // NEW: Title accessibility
+    title: 'Typed Reactive Form'
   },
   {
     path: '**',
